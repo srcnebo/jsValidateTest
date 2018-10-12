cf92ea3e04d7e70af7656c9c88906a07a9d321bc
Steven <stv.nebo@gmail.com>
2018-10-12 18:12:00 +0300

    Valication complete

--- script.js
+++ script.js
@@ -7,21 +7,51 @@ const button = document.querySelector("button");
 
 console.log(inputs);
 
+const inputsToArray = Array.from(inputs);
+
 const patterns = {
-  firstname: /^[a-zA-Z]{3,15}$/,
+  firstname: /Steve/,
   lastname: /^[a-zA-Z]{3,15}$/,
   email: /^[a-z]+[\.]*[a-z]*@[a-z]+\.[a-z]{2,3}[\.]?[a-z]{1,2}$/,
-  password: /^[\w\W\s]{6,20}$/
+  password: /^[\w\W\s]{6,20}$/,
+  telephone: /00/,
+  bio: /hi/
 };
 
-const fName = document.querySelector("#firstname");
-console.log(fName.value);
-if (patterns.firstname.test(fName)) {
-  console.log("true");
+inputs.forEach(element => {
+  element.addEventListener("input", e => {
+    formValidator(e.target, patterns[e.target.name]);
+  });
+});
+
+function isValid() {
+  return inputsToArray.every(element => {
+    return element.className == "valid";
+  });
 }
+const formValidator = (element, pattern) => {
+  if (pattern.test(element.value)) {
+    element.className = "valid";
+    console.log("Hi1");
+    if (isValid()) {
+      button.className = "submit-button";
+      button.disabled = false;
+      console.log("Hi2");
+    } else {
+      button.classList.remove("submit-button");
+      button.disabled = true;
+      console.log("Hi3");
+    }
+  } else {
+    element.className = "invalid";
+    console.log("hi4");
+  }
+};
+
+// inputsToArray.every(formValidator)
 
 // const formValidator = (element, pattern) => {
-//   if (pattern.test(element.value)) {
+//   if () {
 //     element.className = "valid";
 //     button.className = "submit-button";
 //     button.disabled = false;
@@ -31,3 +61,10 @@ if (patterns.firstname.test(fName)) {
 //     button.disabled = true;
 //   }
 // };
+
+//button.disabled = true;
+//button.classList.remove("submit-button");
+//button.className = "submit-button";
+//element.className = "invalid";
+//button.disabled = true;
+//element.className = "valid";
